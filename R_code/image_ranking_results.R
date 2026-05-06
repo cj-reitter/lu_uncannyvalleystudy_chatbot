5458e6667deb8dece0c8ca39359f63d73519ac8a
cj-reitter <carreitter@gmail.com>
2026-05-06 11:58:12 +0200

    Add mad and median calculations to R code
    Add mad and median calculations to R code

--- R_code/image_ranking_results.R
+++ R_code/image_ranking_results.R
@@ -6,23 +6,44 @@ library(ggplot2)
 con <- dbConnect(SQLite(), "../Thesis/lu_uncannyvalleystudy_chatbot/thesis_uv_website/chatbot_database.sqlite3")
 ranking_data <- dbReadTable(con, 'avatar_ranking_imageranking')
 
-# Mean and standard deviation with no normilization
+# Mean and median with no normalization
 ranking_results <- ranking_data %>%
   group_by(image_name) %>%
   summarise(
     mean = mean(ranking),
     sd_z   = sd(ranking),
+    median = median(ranking),
+    mad = mad(ranking),
     n      = n()
   )
 
+# Mean Plots
 ggplot(ranking_results, aes(x = mean)) +
   geom_histogram(
     breaks = seq(10, 100, by = 10),
     color = "white"
   )
 
+ggplot(ranking_results, aes(x = reorder(image_name, mean), y = mean)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = mean - sd_z, ymax = mean + sd_z)
+  )
 
-# Mean and standard deviation with within-user normalization
+# Median Plots
+ggplot(ranking_results, aes(x = median)) +
+  geom_histogram(
+    breaks = seq(10, 100, by = 10),
+    color = "white"
+  )
+
+ggplot(ranking_results, aes(x = reorder(image_name, median), y = median)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = median - mad, ymax = median + mad)
+  )
+
+# Mean with within-rater normalization
 ranking_z_scores <- ranking_data %>%
   group_by(session_id) %>%
   mutate(
@@ -43,14 +64,26 @@ ranking_norm <- ranking_norm %>% filter(!is.na(mean_z))
 
 ranking_norm_results <- ranking_norm %>%
   mutate(
-    bin = cut(
+    mean_bin = cut(
       mean_z,
+      breaks = 10,
+      labels = FALSE
+    ),
+    median_bin = cut(
+      median_z,
       breaks = 10,
       labels = FALSE
     )
   )
 
-ggplot(ranking_norm_results, aes(x = bin)) +
+# Mean Plots
+ggplot(ranking_norm_results, aes(x = mean_bin)) +
   geom_histogram(
     color = "white"
+  )
+
+ggplot(ranking_norm, aes(x = reorder(image_name, mean_z), y = mean_z)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = mean_z - sd_z, ymax = mean_z + sd_z)
   )
