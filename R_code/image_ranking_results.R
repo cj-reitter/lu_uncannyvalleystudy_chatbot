f757db808d72b762ed92c1d2fd27ca758877bd43
cj-reitter <carreitter@gmail.com>
2026-05-09 14:00:29 +0200

    Update R code.
    Fix up filtered rater filtering in the R code

--- R_code/image_ranking_results.R
+++ R_code/image_ranking_results.R
@@ -7,53 +7,12 @@ library(tidyverse)
 library(irr)
 library(psych)
 library(purrr)
+library(ggrepel)
 
 con <- dbConnect(SQLite(), "../Thesis/lu_uncannyvalleystudy_chatbot/thesis_uv_website/chatbot_database.sqlite3")
 ranking_data <- dbReadTable(con, 'avatar_ranking_imageranking')
 
-# Mean and median with no normalization
-ranking_results <- ranking_data %>%
-  group_by(image_name) %>%
-  summarise(
-    mean = mean(ranking),
-    sd_z   = sd(ranking),
-    median = median(ranking),
-    mad = mad(ranking),
-    n      = n()
-  )
-
-# Mean Plots
-ggplot(ranking_results, aes(x = mean)) +
-  geom_histogram(
-    breaks = seq(10, 100, by = 10),
-    color = "white"
-  )
-
-ggplot(ranking_results, aes(x = reorder(image_name, mean), y = mean)) +
-  geom_col(fill = "light grey") +
-  geom_errorbar(
-    aes(ymin = mean - sd_z, ymax = mean + sd_z)
-  )
-
-# Median Plots
-ggplot(ranking_results, aes(x = median)) +
-  geom_histogram(
-    breaks = seq(10, 100, by = 10),
-    color = "white"
-  )
-
-ggplot(ranking_results, aes(x = reorder(image_name, median), y = median)) +
-  geom_col(fill = "light grey") +
-  geom_errorbar(
-    aes(ymin = median - mad, ymax = median + mad)
-  )
-
 # Inter-rater data
-filtered_data <- ranking_data %>%
-  group_by(session_id) %>%
-  filter(n() == 50) %>% 
-  ungroup()
-
 rater_summary <- ranking_data %>%
   group_by(session_id) %>%
   summarise(
@@ -61,9 +20,23 @@ rater_summary <- ranking_data %>%
     rater_median = median(ranking, na.rm = TRUE),
     rater_sd     = sd(ranking, na.rm = TRUE),
     rater_count     = n()
-  )
+  ) %>%
+  dplyr::mutate(rater_index = dplyr::row_number())
 
-individual_ratings <- filtered_data %>%
+
+#filtered_raters <- rater_summary %>%
+#  filter(rater_count >= 10) %>%
+#  select(session_id)
+#
+#filtered_ranking_data <- ranking_data %>%
+#  filter(session_id %in% filtered_raters$session_id)
+
+filtered_ranking_data <- ranking_data %>%
+  group_by(session_id) %>%
+  filter(n() == 50) %>% 
+  ungroup()
+
+individual_ratings <- filtered_ranking_data %>%
   select(session_id, image_name, ranking) %>%
   pivot_wider(
     names_from = session_id,
@@ -71,9 +44,26 @@ individual_ratings <- filtered_data %>%
   )
 
 session_ids <- colnames(individual_ratings)[-1]
-  
+
 # Export rater data
 write_csv(rater_summary, "~/../Thesis/rater_data.csv")
+
+# Rater Mean vs. SD Plot
+ggplot(rater_summary, aes(x = rater_mean, y = rater_sd)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_text_repel(
+    aes(label = rater_index),
+    size = 3,
+    max.overlaps = Inf
+  )
+# Rater Median vs. SD Plot
+ggplot(rater_summary, aes(x = rater_median, y = rater_sd)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_text_repel(
+    aes(label = rater_index),
+    size = 3,
+    max.overlaps = Inf
+  )
 
 # Weighted Kappa
 fleiss_kappa <- kappam.fleiss(ratings_wide[ , -1])
@@ -126,6 +116,96 @@ per_rater_icc <- map_df(session_ids, function(r) {
   )
 })
 
+# Mean and median with no normalization
+
+ranking_results <- ranking_data %>%
+  group_by(image_name) %>%
+  summarise(
+    mean = mean(ranking),
+    sd   = sd(ranking),
+    median = median(ranking),
+    mad = mad(ranking),
+    n      = n()
+  )
+
+filtered_ranking_results <- filtered_ranking_data %>%
+  group_by(image_name) %>%
+  summarise(
+    mean = mean(ranking),
+    sd   = sd(ranking),
+    median = median(ranking),
+    mad = mad(ranking),
+    n      = n()
+  )
+
+# Mean Plots
+ggplot(ranking_results, aes(x = mean)) +
+  geom_histogram(
+    breaks = seq(10, 100, by = 10),
+    color = "white"
+  )
+
+ggplot(filtered_ranking_results, aes(x = mean)) +
+  geom_histogram(
+    breaks = seq(10, 100, by = 10),
+    color = "white"
+  )
+
+ggplot(ranking_results, aes(x = reorder(image_name, mean), y = mean)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = mean - sd, ymax = mean + sd)
+  )
+
+ggplot(filtered_ranking_results, aes(x = reorder(image_name, mean), y = mean)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = mean - sd, ymax = mean + sd)
+  )
+
+# Median Plots
+ggplot(ranking_results, aes(x = median)) +
+  geom_histogram(
+    breaks = seq(10, 100, by = 10),
+    color = "white"
+  )
+
+ggplot(filtered_ranking_results, aes(x = median)) +
+  geom_histogram(
+    breaks = seq(10, 100, by = 10),
+    color = "white"
+  )
+
+ggplot(ranking_results, aes(x = reorder(image_name, median), y = median)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = median - mad, ymax = median + mad)
+  )
+
+ggplot(filtered_ranking_results, aes(x = reorder(image_name, median), y = median)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = median - mad, ymax = median + mad)
+  )
+
+# Mean vs SD Scatter
+ggplot(ranking_results, aes(x = mean, y = sd)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_text_repel(
+    aes(label = image_name),
+    size = 3,
+    max.overlaps = Inf
+  )
+
+# Median vs SD Scatter
+ggplot(ranking_results, aes(x = median, y = sd)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_text_repel(
+    aes(label = image_name),
+    size = 3,
+    max.overlaps = Inf
+  )
+
 # Mean with within-rater normalization
 ranking_z_scores <- ranking_data %>%
   group_by(session_id) %>%
@@ -134,7 +214,22 @@ ranking_z_scores <- ranking_data %>%
   ) %>%
   ungroup()
 
+filtered_ranking_z_scores <- filtered_ranking_data %>%
+  group_by(session_id) %>%
+  mutate(
+    rating_z = (ranking - mean(ranking)) / sd(ranking)
+  ) %>%
+  ungroup()
+
 ranking_norm <- ranking_z_scores %>%
+  group_by(image_name) %>%
+  summarise(
+    mean_z = mean(rating_z),
+    sd_z   = sd(rating_z),
+    n      = n()
+  )
+
+filtered_ranking_norm <- filtered_ranking_z_scores %>%
   group_by(image_name) %>%
   summarise(
     mean_z = mean(rating_z),
@@ -151,9 +246,15 @@ ranking_norm_results <- ranking_norm %>%
       mean_z,
       breaks = 10,
       labels = FALSE
-    ),
-    median_bin = cut(
-      median_z,
+    )
+  )
+
+filtered_ranking_norm <- filtered_ranking_norm %>% filter(!is.na(mean_z))
+
+filtered_ranking_norm_results <- filtered_ranking_norm %>%
+  mutate(
+    mean_bin = cut(
+      mean_z,
       breaks = 10,
       labels = FALSE
     )
@@ -165,8 +266,20 @@ ggplot(ranking_norm_results, aes(x = mean_bin)) +
     color = "white"
   )
 
+ggplot(filtered_ranking_norm_results, aes(x = mean_bin)) +
+  geom_histogram(
+    color = "white"
+  )
+
 ggplot(ranking_norm, aes(x = reorder(image_name, mean_z), y = mean_z)) +
   geom_col(fill = "light grey") +
   geom_errorbar(
     aes(ymin = mean_z - sd_z, ymax = mean_z + sd_z)
   )
+
+ggplot(filtered_ranking_norm, aes(x = reorder(image_name, mean_z), y = mean_z)) +
+  geom_col(fill = "light grey") +
+  geom_errorbar(
+    aes(ymin = mean_z - sd_z, ymax = mean_z + sd_z)
+  )
+
